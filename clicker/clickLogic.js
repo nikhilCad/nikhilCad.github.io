62f2925a99f7ee7b3371702bd8a175ab7bc6dba5
nikhilCad <47214394+nikhilCad@users.noreply.github.com>
2019-10-24 15:13:11 +0530

    Add files via upload

--- clicker/clickLogic.js
+++ clicker/clickLogic.js
@@ -1,6 +1,23 @@
 var money=0;
+var cursors=0;
 
 function moneyClick(num){
  money=money+num; 
  document.getElementById("moneyText").innerHTML=money
 };
+
+function buyCursor(){
+	var cursorCost=Math.floor(10*Math.pow(1.1,cursors));
+	if (money>=cursorCost){
+			money-=cursorCost;
+			document.getElementById("moneyText").innerHTML=money;
+			cursors+=1;
+			document.getElementById("cursorCount").innerHTML=cursors;	
+	};
+	var cursorCost=Math.floor(10*Math.pow(1.1,cursors));
+	document.getElementById("cursorCost").innerHTML=cursorCost;		
+};
+
+window.setInterval(function(){
+	moneyClick(cursors);
+},1000);
