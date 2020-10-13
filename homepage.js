b59fdc0c225042efc67398efa23bf4ab09dcaba3
Nikhil Kadiyan <nikhilcaddilac@gmail.com>
2020-10-13 13:13:40 +0530

    the dog now rotates with time!!!!

--- homepage.js
+++ homepage.js
@@ -9,14 +9,24 @@ let app= new Application({width:256,height:256});
 document.body.appendChild(app.view);
 loader.add("images/dog.png").load(setup);
 
+let timeFromStart = 0;
+let dog;
+
  function setup(){
-    let dog= new Sprite(loader.resources["images/dog.png"].texture);
+    dog= new Sprite(loader.resources["images/dog.png"].texture);
     dog.x=100;  dog.y=100;
     dog.scale.x=4;  dog.scale.y=4;
     dog.anchor.x=0.5;  dog.anchor.y=0.5;
     dog.rotation=0.5;
     app.stage.addChild(dog);
+
+    app.ticker.add(delta=> gameLoop(delta))//60 FPS
  }
+
+function gameLoop(delta){
+	timeFromStart = timeFromStart + 1;
+	dog.rotation = Math.sin(timeFromStart)*0.1;
+}
 
 //Math.abs(Math.sin(num))
 
