93b4d5dced58449dd8852f5d620798f776cf53d3
Nikhil Kadiyan <nikhilcaddilac@gmail.com>
2020-10-13 13:06:16 +0530

    some more minor changes

--- homepage.js
+++ homepage.js
@@ -11,9 +11,15 @@ loader.add("images/dog.png").load(setup);
 
  function setup(){
     let dog= new Sprite(loader.resources["images/dog.png"].texture);
-    dog.x=50;
-    dog.y=50;
-    dog.scale.x=4;
-    dog.scale.y=4;
+    dog.x=100;  dog.y=100;
+    dog.scale.x=4;  dog.scale.y=4;
+    dog.anchor.x=0.5;  dog.anchor.y=0.5;
+    dog.rotation=0.5;
     app.stage.addChild(dog);
  }
+
+//Math.abs(Math.sin(num))
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
