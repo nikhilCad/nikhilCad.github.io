324aa4f1d055797e6696eb132a041ddf5b04d571
Nikhil Kadiyan <nikhilcaddilac@gmail.com>
2020-10-13 13:33:39 +0530

    dog has gone mad!!!

--- homepage.js
+++ homepage.js
@@ -14,7 +14,7 @@ let dog;
 
  function setup(){
     dog= new Sprite(loader.resources["images/dog.png"].texture);
-    dog.x=100;  dog.y=100;
+    dog.x=100;  dog.y=120;
     dog.scale.x=4;  dog.scale.y=4;
     dog.anchor.x=0.5;  dog.anchor.y=0.5;
     dog.rotation=0.5;
@@ -30,6 +30,7 @@ function gameLoop(){
 	timeFromStart = timeFromStart + 1;
 	dog.rotation = 3.14159*Math.sin(timeFromStart*0.05);
 	dog.x += Math.sin(timeFromStart*0.05);
+	dog.y += 4*Math.sin(timeFromStart*0.25);
 }
 
 gameLoop();//calling it for first time here
