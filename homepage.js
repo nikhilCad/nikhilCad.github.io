a3197894fbf323ddba8a021204c09ec9569552ec
Nikhil Kadiyan <nikhilcaddilac@gmail.com>
2020-10-13 12:33:43 +0530

    add aliases

--- homepage.js
+++ homepage.js
@@ -1,8 +1,14 @@
-let app= new PIXI.Application({width:256,height:256});
+let Application = PIXI.Application,
+	loader = PIXI.loader,
+	Sprite = PIXI.Sprite;
+
+let app= new Application({width:256,height:256});
 document.body.appendChild(app.view);
-PIXI.loader.add("images/dog.png").load(setup);
+loader.add("images/dog.png").load(setup);
 
  function setup(){
-    let dog= new PIXI.Sprite(PIXI.loader.resources["images/dog.png"].texture);
+    let dog= new Sprite(loader.resources["images/dog.png"].texture);
+    dog.x=100;
+    dog.y=100;
     app.stage.addChild(dog);
  }
