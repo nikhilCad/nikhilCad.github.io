3b10928b5338ec6cea194931d7c38eb746018212
Nikhil Kadiyan <47214394+nikhilCad@users.noreply.github.com>
2021-03-21 20:48:17 +0530

    Remove red rectangle

--- homepage.js
+++ homepage.js
@@ -26,11 +26,6 @@ let box=null;
     dog.rotation=0.5;
     app.stage.addChild(dog);
 
-    box = new Sprite(loader.resources["images/box.png"].texture);
-    box.x=-10;  box.y=60;
-    box.scale.x=100;box.scale.y=20;
-    app.stage.addChild(box);
-
     let style = new TextStyle({
 				  fontFamily: "Arial",
 				  fontSize: 36,
@@ -56,11 +51,6 @@ function gameLoop(){
 	dog.x += Math.sin(timeFromStart*0.05);
 	dog.y += 4*Math.sin(timeFromStart*0.25);
 
-	if (hitTestRectangle(dog, box)) {
-    box.tint = 0xccff99;//green
-	} else {
-    box.tint = 0xff0000;//red
-	}
 }
 
 gameLoop();//calling it for first time here
@@ -127,55 +117,3 @@ function keyboard(value) {
   
   return key;
 }
-
-//Copy-Paste function
-function hitTestRectangle(r1, r2) {
-
-  //Define the variables we'll need to calculate
-  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
-
-  //hit will determine whether there's a collision
-  hit = false;
-
-  //Find the center points of each sprite
-  r1.centerX = r1.x + r1.width / 2;
-  r1.centerY = r1.y + r1.height / 2;
-  r2.centerX = r2.x + r2.width / 2;
-  r2.centerY = r2.y + r2.height / 2;
-
-  //Find the half-widths and half-heights of each sprite
-  r1.halfWidth = r1.width / 2;
-  r1.halfHeight = r1.height / 2;
-  r2.halfWidth = r2.width / 2;
-  r2.halfHeight = r2.height / 2;
-
-  //Calculate the distance vector between the sprites
-  vx = r1.centerX - r2.centerX;
-  vy = r1.centerY - r2.centerY;
-
-  //Figure out the combined half-widths and half-heights
-  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
-  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-  //Check for a collision on the x axis
-  if (Math.abs(vx) < combinedHalfWidths) {
-
-    //A collision might be occurring. Check for a collision on the y axis
-    if (Math.abs(vy) < combinedHalfHeights) {
-
-      //There's definitely a collision happening
-      hit = true;
-    } else {
-
-      //There's no collision on the y axis
-      hit = false;
-    }
-  } else {
-
-    //There's no collision on the x axis
-    hit = false;
-  }
-
-  //`hit` will be either `true` or `false`
-  return hit;
-};
