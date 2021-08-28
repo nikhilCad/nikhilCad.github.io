330b01f04cf78b9d4a64f87983ff4ac0a86dee48
nikhilcad <nikhilcaddilac@gmail.com>
2021-08-28 21:05:55 +0530

    codechef LTIME99C: PROGLANG

--- test.py
+++ test.py
@@ -1,5 +1,34 @@
-#this file is for codechef
-n=int(input())
+t = int(input())#test case
 
-if n%2==0 and n>=2 and n<=100:
-    print(int(n*n/2))
+if t>=1 and t<=288:
+
+    arr=[]
+
+    for i in range(t): 
+
+        inptemp = input()
+        arr.append(inptemp)
+
+for inp in arr:
+    inp= inp.split()
+    if len(inp)==6:
+
+        for i in inp:
+            if int(i)>=1 and int(i)<=4:
+                if inp[0]!=inp[1]:
+                    if inp[2]!=inp[3] and inp[3]!=inp[4] and inp[4]!=inp[5]:
+                        pass
+                    else:
+                        break
+
+        if inp[0] in [inp[2],inp[3]] and inp[1] in [inp[2],inp[3]]:
+            print(1)
+            continue
+        if inp[0] in [inp[4],inp[5]] and inp[1] in [inp[4],inp[5]]:
+            print(2)
+            continue
+        else:
+            print(0)
+
+
+                
